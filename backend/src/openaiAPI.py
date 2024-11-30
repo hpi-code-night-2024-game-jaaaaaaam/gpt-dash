592c355770648551e90d48fd4833a7c04522ff1e
MrEritra <mstick2007@gmail.com>
2024-11-30 01:30:22 +0100

    Random Prompts

--- backend/src/openaiAPI.py
+++ backend/src/openaiAPI.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 from openai import OpenAI
 import os
+from pathlib import Path
+import random
+
 
 load_dotenv()
 api_key = os.getenv("MY_VERY_SECRET_KEY")
@@ -10,23 +13,26 @@ client = OpenAI (
 )
 
 def test():
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role":"system", "content":"You are playing a game where the humans try to write responses that sound similar to Answers by large language models. Then they will be shown the answers everybody wrote and one answer by a large language model is added, this is your answer. The players vote on which answer was written by you the large language model. Make the answer such that it is possible to guess but not easy. Limit responses to 3 sentences."},
-            {"role":"user", "content":"Stell dir vor, Schokolade hätte Gefühle – was würde sie über Diäten sagen?"}
-        ]
-    )
-    print(completion.choices[0].message.content)
+    print(send_rand_req())
+    
+# lines = Path("file.txt").read_text().strip().splitlines()
+# randomPrompt = random.choice(lines)
+
+def send_rand_req()->str:
+    lines =  Path("PROMPTS_LIST.txt").read_text().strip().splitlines()
+    randomPrompt = random.choice(lines)
+    return send_req(req=randomPrompt)
+    
 
 def send_req(req:str):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role":"system", "content":"You are playing a game where the humans try to write responses that sound similar to Answers by large language models. Then they will be shown the answers everybody wrote and one answer by a large language model is added, this is your answer. The players vote on which answer was written by you the large language model. Make the answer such that it is possible to guess but not easy. Limit responses to 3 sentences."},
+            {"role":"system", "content":"You are playing a game where the humans try to write responses that sound similar to Answers by large language models. Then they will be shown the answers everybody wrote and one answer by a large language model is added, this is your answer. The players vote on which answer was written by you the large language model. Make the answer such that it is possible to guess but not easy. Limit responses to 1 sentence."},
             {"role":"user", "content":req}
         ]
     )
+    return completion.choices[0].message.content
 
 
 if __name__ == '__main__':
