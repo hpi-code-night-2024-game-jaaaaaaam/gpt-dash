d1abbedb6731cdf7a8dacf730e95a16b4973fd9f
MrEritra <mstick2007@gmail.com>
2024-11-30 01:06:52 +0100

    Requirements + openai API

---
+++ openaitest/openaiAPI.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+api_key = os.getenv("MY_VERY_SECRET_KEY")
+
+client = OpenAI (
+    api_key=api_key
+)
+
+def test():
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role":"system", "content":"You are playing a game where the humans try to write responses that sound similar to Answers by large language models. Then they will be shown the answers everybody wrote and one answer by a large language model is added, this is your answer. The players vote on which answer was written by you the large language model. Make the answer such that it is possible to guess but not easy. Limit responses to 3 sentences."},
+            {"role":"user", "content":"Stell dir vor, Schokolade hätte Gefühle – was würde sie über Diäten sagen?"}
+        ]
+    )
+    print(completion.choices[0].message.content)
+
+def send_req(req:str):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role":"system", "content":"You are playing a game where the humans try to write responses that sound similar to Answers by large language models. Then they will be shown the answers everybody wrote and one answer by a large language model is added, this is your answer. The players vote on which answer was written by you the large language model. Make the answer such that it is possible to guess but not easy. Limit responses to 3 sentences."},
+            {"role":"user", "content":req}
+        ]
+    )
+
+
+if __name__ == '__main__':
+    test()
